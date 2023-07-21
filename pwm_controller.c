605cda9db6f327b81b5288a6d3378c9c6e499be2
VanLocNguyen1999 <Locnv@selex.vn>
2023-07-21 15:58:41 +0700

    test

--- pwm_controller.c
+++ pwm_controller.c
@@ -1,16 +1,15 @@
 #include "pwm_controller.h"
 
 void PWMController_Init(PWMController *pwm, TIM_HandleTypeDef *htim, uint32_t channel, uint32_t frequency, float dutyCycle) {
-
+    int a;
+	pwm->htim = htim;
+	pwm->htim->Instance->ARR = (HAL_RCC_GetPCLK2Freq() / frequency  );
     uint32_t timerClock = HAL_RCC_GetPCLK2Freq(); // Lấy tần số clock của Timer
     uint32_t prescaler = timerClock / frequency - 1;
     uint32_t pulse = (pwm->htim->Instance->ARR + 1) * dutyCycle;
-    //
-    pwm->htim = htim;
     pwm->channel = channel;
-    pwm->htim->Instance->PSC = prescaler;
+    pwm->htim->Init.Prescaler = prescaler;
     pwm->htim->Instance->CCR1 = pulse;
-    //HAL_TIM_PWM_Start(pwm->htim, pwm->channel);
+    HAL_TIM_PWM_Start(pwm->htim, pwm->channel);
 }
-
 
